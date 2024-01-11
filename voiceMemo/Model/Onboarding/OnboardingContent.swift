52f039b2b9c4343569d211a6939e5aa00b60cac4
thinkySide <eunlyuing@gmail.com>
2024-01-11 22:43:48 +0900

    2-2. 온보딩 화면 구현하기

--- voiceMemo/Model/Onboarding/OnboardingContent.swift
+++ voiceMemo/Model/Onboarding/OnboardingContent.swift
@@ -5,5 +5,19 @@
 
 import Foundation
 
-struct OnboardingContent {
+/// 탭뷰로 사용되기 때문에 Hashable 프로토콜 채택
+struct OnboardingContent: Hashable {
+    var imageFileName: String
+    var title: String
+    var subTitle: String
+    
+    init(
+        imageFileName: String,
+        title: String,
+        subTitle: String
+    ) {
+        self.imageFileName = imageFileName
+        self.title = title
+        self.subTitle = subTitle
+    }
 }
